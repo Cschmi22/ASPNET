910cf5a7cabd18d1a6be09c5eb7eb7e6b72cf8aa
Caleb Schmidt <cschmi14@gmail.com>
2023-04-13 11:58:04 -0400

    done

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using System.Collections;
+using Testing.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
